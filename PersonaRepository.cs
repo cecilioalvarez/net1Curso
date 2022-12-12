3f1591ce35d79f53678278323a90816f507b823b
cecilioAlvarez <cmac@arquitecturajava.com>
2022-12-12 12:28:03 +0100

    ultimos

---
+++ PersonaRepository.cs
@@ -0,0 +1,28 @@
+using MySql.Data.MySqlClient;
+
+namespace net1
+{
+    public class PersonaRepository {
+
+
+        public Connection obtenerConexion() {
+
+
+            var sb = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                UserID = "root",
+                Password = "root",
+                Port = 3306,
+                Database = "curso2"
+            };
+            MySqlConnection conn = 
+            new MySqlConnection(sb.ConnectionString);
+            conn.Open();
+            return conn;
+
+
+        }
+
+    }  
+ }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace net1
         static void Main(string[] args)
         {
 
-             var sb = new MySqlConnectionStringBuilder
+            var sb = new MySqlConnectionStringBuilder
             {
                 Server = "localhost",
                 UserID = "root",
@@ -17,7 +17,20 @@ namespace net1
                 Port = 3306,
                 Database = "curso2"
             };
+            MySqlConnection conn = 
+            new MySqlConnection(sb.ConnectionString);
+            conn.Open();
 
+            var comando = conn.CreateCommand();
+            comando.CommandText = "select * from  Personas";
+            var reader = 
+            comando
+            .ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            while(reader.Read()) {
+
+
+                Console.WriteLine(reader.GetString("nombre"));
+            }
 
          Console.WriteLine("hola");
         }
