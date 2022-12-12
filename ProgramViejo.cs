6d6a99fb46ce00b24e23d6156f7da60aaeb5fb06
cecilioAlvarez <cmac@arquitecturajava.com>
2022-12-12 09:45:22 +0100

    nuevos

---
+++ ProgramViejo.cs
@@ -0,0 +1,98 @@
+﻿    /*
+using System;
+using net1;
+namespace net1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("hola mundo");
+            //numero entero
+            int numero=7;
+            String cadena="hola";
+
+            Console.WriteLine(numero);
+            if(numero>5) {
+                Console.WriteLine("has aprobado");
+            }else{
+                Console.WriteLine("has suspendido");
+            }
+            Console.WriteLine(cadena);
+            */
+            /*
+            for (int i=0;i<10;i++) {
+
+                Console.WriteLine("hola"+i);
+            }
+            int j=0;
+
+            while (j<10) {
+                Console.WriteLine("hola"+j);
+                j++;
+            }
+           
+            int[] lista= new int[] {1,3,7,8,1};
+
+            for (int i=0;i<lista.Length;i++) {
+
+                Console.WriteLine(lista[i]);
+            }
+            
+            String [] lista2 = new String[] {"hola","que","tal"};
+
+
+            for (int i=0;i<lista2.Length;i++) {
+
+                Console.WriteLine(lista2[i]);
+            }
+
+            
+
+             Persona p= new Persona();
+             p.nombre="pedro";
+             p.edad=70;
+             Console.WriteLine(p.nombre);
+             Console.WriteLine(p.edad);
+             Console.WriteLine(p.estaJubilado());
+            */
+
+            /*
+             Factura f= new Factura();
+             f.numero=1;
+             f.concepto="tv";
+             f.importe=200;
+             f.pagada=false;
+             if(f.pagada) {
+                Console.WriteLine(f.ImporteConIVA());
+
+             }else {
+                Console.WriteLine("no esta pagada");
+             }
+           
+
+           Rectangulo o1= new Rectangulo();
+           o1.lado1=2;
+           o1.lado2=4;
+           double area= o1.Area();
+           Console.WriteLine(area);
+          
+            Caja c= new Caja(2,3,8);
+            //c.ancho=2;
+            //c.alto=3;
+            //c.profundo=6;
+            Console.WriteLine(c.Volumen());
+
+           
+             Persona objeto1= new Persona();
+             objeto1.Nombre="pedro";
+             objeto1.Edad=500;
+             Console.WriteLine(objeto1.Edad);
+             Console.WriteLine(objeto1.Nombre);
+
+        }
+    }
+}
+
+  */
