568d8643b59332c52548bcf19df8a12f6b29458e
cecilioAlvarez <cmac@arquitecturajava.com>
2022-12-12 10:23:58 +0100

    libreria

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿
 using System;
 using net1;
+using MySql.Data.MySqlClient;
 namespace net1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+             var sb = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                UserID = "root",
+                Password = "root",
+                Port = 3306,
+                Database = "curso2"
+            };
+
+
          Console.WriteLine("hola");
         }
     }
