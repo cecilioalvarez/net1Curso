df470e7a70b8c4ed210b142dc4d64e53fdfb3ffe
cecilioAlvarez <cmac@arquitecturajava.com>
2022-12-13 09:27:24 +0100

    ultimos

--- PersonaNueva.cs
+++ PersonaNueva.cs
@@ -6,7 +6,8 @@ namespace net1 {
         public string Nombre {get;set;}
         public string Apellidos {get;set;}
 
-        public PersonaNueva(string dni, string nombre, string apellidos) {
+        public PersonaNueva(string dni,
+         string nombre, string apellidos) {
 
 
             this.Dni=dni;
